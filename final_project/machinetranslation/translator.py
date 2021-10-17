e7dacd0a4c1885a64bb64d15bc104ff17da1cba4
jozicode <jozi0089@gmail.com>
2021-10-17 18:16:00 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,27 +9,26 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
-authenticator = IAMAuthenticator('{apikey}')
+authenticator = IAMAuthenticator('5wXMaUmTXxkXcJiTwYVplrrs72eNwrSDUnmp-Lujj90g')
 language_translator = LanguageTranslatorV3(
-    version='{version}',
+    version='2018-05-01',
     authenticator=authenticator
 )
 
 language_translator.set_service_url('https://api.eu-gb.language-translator.watson.cloud.ibm.com')
 
+#translate english to french
+
 def englishToFrench(englishText):
-    #write the code here
-    frenchText = language_translator.translate(
-    text=englishText,
-    model_id='en-fr').get_result()
+    translation=language_translator.translate(text=englishText,model_id='en-fr').get_result() 
+
+    frenchText=translation['translations'][0]['translation']
     return frenchText
 
+#translate french to english
+
 def frenchToEnglish(frenchText):
-    #write the code here
-    englishText = language_translator.translate(
-    text=frenchText,
-    model_id='fr-en').get_result()
+    translation=language_translator.translate(text=frenchText,model_id='fr-en').get_result() 
+
+    englishText=translation['translations'][0]['translation']
     return englishText
